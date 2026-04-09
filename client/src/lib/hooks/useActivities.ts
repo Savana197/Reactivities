327f22fa3f29a783cc24b59dcba2bad21df1ca21
Savo Ruzicic <ruzicic.savo01@gmail.com>
2026-04-09 05:21:58 +0200

    Implemented routing

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
 
     const queryClient = useQueryClient();
 
@@ -12,6 +12,14 @@ export const useActivities = () => {
             const response = await agent.get<Activity[]>('/activities');
             return response.data;
         }
+    })
+    const {data: activity, isLoading: isLoadingActivity} = useQuery({
+        queryKey: ['acitvities', id],
+        queryFn: async () => {
+            const response = await agent.get<Activity>(`/activities/${id}`)
+            return response.data;
+        },
+        enabled: !!id
     })
 
     const updateActivity = useMutation({
@@ -26,7 +34,8 @@ export const useActivities = () => {
     })
     const createActivity = useMutation({
         mutationFn: async (activity: Activity) => {
-            await agent.post('/activities', activity)
+            const response = await agent.post('/activities', activity)
+            return response.data;
         },
         onSuccess: async () => {
             await queryClient.invalidateQueries({
@@ -48,5 +57,5 @@ export const useActivities = () => {
 
 
 
-    return {activities, isPending, updateActivity, createActivity, deleteActivity}
+    return {activities, isPending, updateActivity, createActivity, deleteActivity, activity, isLoadingActivity}
 }
